7e75017ba3d67afc841f59365940c22aae9baca2
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-05-23 20:13:18 -0400

    Departments
    -Added function to see all the departments
    -Added function to add deepartments
    -Addded function to add role

--- server.js
+++ server.js
@@ -93,3 +93,13 @@ function start() {
             }
         });
 }
+// function to view all departments
+function viewAllDepartments() {
+    const query = "SELECT * FROM departments";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        // restart the application
+        start();
+    });
+}
